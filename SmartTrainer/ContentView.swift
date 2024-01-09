07e765ac86cda025d0a9583ab96706da7477299c
jackomearadexgreen <127737379+jackomearadexgreen@users.noreply.github.com>
2024-01-09 16:49:30 +0000

    Initial Commit

---
+++ SmartTrainer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmartTrainer
+//
+//  Created by Jack O'Meara on 09/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SmartTrainer/SmartTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartTrainerApp.swift
+//  SmartTrainer
+//
+//  Created by Jack O'Meara on 09/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SmartTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
