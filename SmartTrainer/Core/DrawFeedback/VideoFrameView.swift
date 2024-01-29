112c3728d1266837270ea6d47f5175d70e41854a
jackomearadexgreen <127737379+jackomearadexgreen@users.noreply.github.com>
2024-01-29 09:27:39 +0000

    changes before xcode reinstall

---
+++ SmartTrainer/Core/DrawFeedback/VideoFrameView.swift
@@ -0,0 +1,18 @@
+//
+//  VideoFrameView.swift
+//  SmartTrainer
+//
+//  Created by Jack O'Meara on 28/01/2024.
+//
+
+import SwiftUI
+
+struct VideoFrameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideoFrameView()
+}
